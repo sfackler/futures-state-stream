e4a5fc5925011e253d44b3e5a68367fa4e998acb
Steven Fackler <sfackler@gmail.com>
2016-12-22 15:55:22 -0500

    Basic setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,73 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+extern crate futures;
+
+use futures::{Async, Poll, Future, Stream};
+
+pub enum StreamEvent<I, S> {
+    Next(I),
+    Done(S),
+}
+
+pub trait StateStream {
+    type Item;
+    type State;
+    type Error;
+
+    fn poll(&mut self) -> Poll<StreamEvent<Self::Item, Self::State>, Self::Error>;
+
+    #[inline]
+    fn into_future(self) -> IntoFuture<Self>
+        where Self: Sized
+    {
+        IntoFuture(Some(self))
+    }
+}
+
+pub fn stream<S>(stream: S) -> FromStream<S>
+    where S: Stream
+{
+    FromStream(stream)
+}
+
+pub struct FromStream<S>(S);
+
+impl<S> StateStream for FromStream<S>
+    where S: Stream
+{
+    type Item = S::Item;
+    type State = ();
+    type Error = S::Error;
+
+    #[inline]
+    fn poll(&mut self) -> Poll<StreamEvent<S::Item, ()>, S::Error> {
+        self.0.poll().map(|a| {
+            match a {
+                Async::Ready(Some(i)) => Async::Ready(StreamEvent::Next(i)),
+                Async::Ready(None) => Async::Ready(StreamEvent::Done(())),
+                Async::NotReady => Async::NotReady,
+            }
+        })
+    }
+}
+
+pub struct IntoFuture<S>(Option<S>);
+
+impl<S> Future for IntoFuture<S>
+    where S: StateStream
+{
+    type Item = (StreamEvent<S::Item, S::State>, S);
+    type Error = (S::Error, S);
+
+    #[inline]
+    fn poll(&mut self) -> Poll<(StreamEvent<S::Item, S::State>, S), (S::Error, S)> {
+        let item = match self.0.as_mut().expect("polling IntoFuture twice").poll() {
+            Ok(Async::NotReady) => return Ok(Async::NotReady),
+            Ok(Async::Ready(i)) => Ok(i),
+            Err(e) => Err(e),
+        };
+        let stream = self.0.take().unwrap();
+        match item {
+            Ok(i) => Ok(Async::Ready((i, stream))),
+            Err(e) => Err((e, stream)),
+        }
     }
 }
